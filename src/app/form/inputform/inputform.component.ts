7beeecfe2ee7be9f31a83a0fbe66326b808c3e96
DeshaniMAWD <68700581+DeshaniMAWD@users.noreply.github.com>
2021-12-21 12:52:57 +0530

    Add files via upload

---
+++ src/app/form/inputform/inputform.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inputform',
+  templateUrl: './inputform.component.html',
+  styleUrls: ['./inputform.component.css']
+})
+export class InputformComponent implements OnInit, OnDestroy {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  
+
+  ngOnDestroy(): void {
+      
+  }
+
+}
