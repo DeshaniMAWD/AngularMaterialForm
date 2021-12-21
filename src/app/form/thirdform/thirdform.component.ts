15f0acfbfba1613dac22e291b31e56d8058a7b6d
DeshaniMAWD <68700581+DeshaniMAWD@users.noreply.github.com>
2021-12-21 12:55:39 +0530

    Add files via upload

---
+++ src/app/form/thirdform/thirdform.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-thirdform',
+  templateUrl: './thirdform.component.html',
+  styleUrls: ['./thirdform.component.css']
+})
+export class ThirdformComponent implements OnInit, OnDestroy {
+
+   
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  ngOnDestroy(): void {
+      
+  }
+
+}
