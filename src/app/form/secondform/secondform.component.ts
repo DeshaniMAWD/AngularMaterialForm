43c5092ec9beefb9c1eebe3120a09076075f4c7f
DeshaniMAWD <68700581+DeshaniMAWD@users.noreply.github.com>
2021-12-21 12:54:56 +0530

    Add files via upload

---
+++ src/app/form/secondform/secondform.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-secondform',
+  templateUrl: './secondform.component.html',
+  styleUrls: ['./secondform.component.css']
+})
+export class SecondformComponent implements OnInit, OnDestroy {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+      
+  }
+}
