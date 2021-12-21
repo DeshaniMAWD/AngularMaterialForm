8fdd9a37f253c7afb163f844659b7c9232e31506
DeshaniMAWD <68700581+DeshaniMAWD@users.noreply.github.com>
2021-12-21 12:54:03 +0530

    Add files via upload

---
+++ src/app/form/mha101mainfile/mha101mainfile.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mha101mainfile',
+  templateUrl: './mha101mainfile.component.html',
+  styleUrls: ['./mha101mainfile.component.css']
+})
+export class FirstformComponent implements OnInit,OnDestroy {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  
+  ngOnDestroy(): void {
+      
+  }
+
+}
