3f2c3c9cf25bc52c422bcde4113559aac7608be8
DeshaniMAWD <68700581+DeshaniMAWD@users.noreply.github.com>
2021-12-21 13:00:16 +0530

    Add files via upload

---
+++ src/app/form/lastform/lastform.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lastform',
+  templateUrl: './lastform.component.html',
+  styleUrls: ['./lastform.component.css']
+})
+export class LastformComponent implements OnInit, OnDestroy {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  ngOnDestroy(): void {
+      
+  }
+
+}
